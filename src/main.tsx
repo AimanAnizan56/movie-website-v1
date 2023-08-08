99c37df0c7a7a5d5189cf95c5aa9e907abf51998
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-08 21:51:57 +0800

    Add route for movie with defined loader

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,29 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './routes/App.tsx';
 import './index.css';
 import ErrorPage from './error-page.tsx';
+import Movie from './routes/Movie.tsx';
+import { client } from './lib/sanity.query.ts';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: '/movie/:movieSlug',
+    element: <Movie />,
+    loader: async ({ params }) => {
+      return await client.fetch(`*[_type=='movie' && slug.current=='${params.movieSlug}' && !(_id match "drafts*")] {
+        castMembers[]{ characterName, 'realName': person->name, "imageUrl": person->image.asset->url },
+        crewMembers[]{ job, 'name': person->name },
+        'overview': overview[0].children[0].text,
+        popularity,
+        'posterAsset': poster.asset->url,
+        releaseDate,
+        title,
+      }`);
+    },
   },
 ]);
 
