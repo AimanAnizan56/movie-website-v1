2db67ba2e759781930b84da85c8bbe59f4bb5607
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-08 23:13:09 +0800

    Add cast and crew members

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -30,8 +30,8 @@ const Movie = () => {
   const MovieName = styled.h1`
     text-decoration: underline;
     text-align: center;
-    max-width: 600px;
     @media (max-width: 1000px) {
+      max-width: 600px;
       margin: 40px auto;
     }
     @media (min-width: 1000px) {
@@ -53,6 +53,7 @@ const Movie = () => {
   `;
   const Poster = styled.img`
     width: 400px;
+    height: 600px;
   `;
 
   const DetailContainer = styled.div`
@@ -66,9 +67,37 @@ const Movie = () => {
     align-items: center;
   `;
 
+  const CastContainer = styled.div`
+    display: grid;
+    grid-template-columns: auto auto;
+    gap: 10px;
+    @media (min-width: 1000px) {
+      grid-template-columns: auto;
+    }
+    @media (min-width: 1180px) {
+      grid-template-columns: auto auto;
+    }
+    @media (min-width: 1680px) {
+      grid-template-columns: auto auto auto;
+    }
+  `;
+  const Cast = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    align-items: center;
+    > img {
+      width: 50px;
+      height: 70px;
+      border-radius: 5px;
+    }
+  `;
+
   return (
     <Container>
-      <MovieName>{movie.title}</MovieName>
+      <MovieName>
+        {movie.title} ({new Date(movie.releaseDate).getFullYear()})
+      </MovieName>
       <MovieInfo>
         <PosterContainer>
           <Poster src={movie.posterAsset} alt={movie.title} />
@@ -78,9 +107,36 @@ const Movie = () => {
             <img src={starLogo} alt="star" width={25} height={25} />
             {movie.popularity.toFixed(2)}
           </Popularity>
+          <hr />
           <div>
             <p style={{ fontWeight: 'bold' }}>Overview</p>
             <p>{movie.overview}</p>
+          </div>
+          <hr />
+          <div>
+            <p style={{ fontWeight: 'bold' }}>Top Cast</p>
+            <CastContainer>
+              {movie.castMembers.map((cast, index) => (
+                <Cast key={index}>
+                  <img src={cast.imageUrl} alt={cast.realName} />
+                  <div>
+                    <p style={{ fontWeight: 'bold' }}>{cast.realName}</p>
+                    <p>{cast.characterName}</p>
+                  </div>
+                </Cast>
+              ))}
+            </CastContainer>
+          </div>
+          <hr />
+          <div>
+            <p style={{ fontWeight: 'bold' }}>Crew Members</p>
+            <p>
+              {movie.crewMembers.map((crew, index) => {
+                let name = crew.name;
+                if (index + 1 != movie.crewMembers.length) name += ', ';
+                return <span key={index}>{name}</span>;
+              })}
+            </p>
           </div>
         </DetailContainer>
       </MovieInfo>
